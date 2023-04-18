4e515dc206d8c6f62816d72ecf3204d7ade28a20
Jasper-Matheussen <matheussenjasper@icloud.com>
2023-04-18 20:23:19 +0200

    Added marker for the current location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
+import "package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart";
 import "package:latlong2/latlong.dart";
 import 'package:location/location.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
+      title: "ParkU",
       home: HomeScreen(),
     );
   }
@@ -65,11 +66,38 @@ class HomeScreen extends StatelessWidget {
                           onSourceTapped: null,
                         ),
                       ],
-                      children: [
+                      children: <Widget> [
                         TileLayer(
                           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                           userAgentPackageName: 'com.example.app',
                         ),
+                        MarkerClusterLayerWidget(options: MarkerClusterLayerOptions(
+                          maxClusterRadius: 120,
+                          size: const Size(40, 40),
+                          fitBoundsOptions: const FitBoundsOptions(
+                            padding: EdgeInsets.all(50),
+                          ),
+                          markers: [
+                            Marker(
+                              width: 80.0,
+                              height: 80.0,
+                              point: LatLng(snapshot.data!.latitude!, snapshot.data!.longitude!),
+                              builder: (ctx) =>
+                                  const Icon(Icons.location_on),
+                            ),
+                          ],
+                          polygonOptions: PolygonOptions(
+                            borderColor: Colors.blueAccent,
+                            color: Colors.blueAccent.withOpacity(0.3),
+                            borderStrokeWidth: 3,
+                          ),
+                          builder: (context, markers) {
+                            return FloatingActionButton(
+                              onPressed: null,
+                              child: Text(markers.length.toString()),
+                            );
+                          },
+                        )),
                       ],
                     )
                   ],
@@ -77,7 +105,7 @@ class HomeScreen extends StatelessWidget {
               ),
             );
           } else {
-            return Scaffold(
+            return const Scaffold(
               body: Center(
                 child: CircularProgressIndicator(),
               ),

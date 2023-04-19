f638ea3263ef4166bc0e8cec399c242ba7cd89b4
Jasper-Matheussen <matheussenjasper@icloud.com>
2023-04-19 17:01:07 +0200

    Added signup password hashed

--- lib/signupPage.dart
+++ lib/signupPage.dart
@@ -1,6 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hash/hash.dart';
+
+/// hex encode
+String encodeHEX(List<int> bytes) {
+  var str = '';
+  for (var i = 0; i < bytes.length; i++) {
+    var s = bytes[i].toRadixString(16);
+    str += s.padLeft(2 - s.length, '0');
+  }
+  return str;
+}
+
+/// hex decode
+List<int> decodeHEX(String hex) {
+  var bytes = <int>[];
+  var len = hex.length ~/ 2;
+  for (var i = 0; i < len; i++) {
+    bytes.add(int.parse(hex.substring(i * 2, i * 2 + 2), radix: 16));
+  }
+  return bytes;
+}
 
 class SignupPage extends StatefulWidget {
   @override
@@ -61,7 +84,8 @@ class _SignupPageState extends State<SignupPage> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                // Your sign up logic here
+                // Your sign up logic her
+
                 String username = usernameController.text.trim();
                 String email = emailController.text.trim();
                 String password = passwordController.text;
@@ -72,12 +96,18 @@ class _SignupPageState extends State<SignupPage> {
                   return;
                 }
 
+                // hash the password
+                var bytes = utf8.encode(password);
+                var sha256 = SHA256();
+                var digest = sha256.update(bytes).digest();
+                String hashedPassword = encodeHEX(digest);
+
                 // create a new document in the 'users' collection
                 try {
                   await widget.firestore.collection('users').add({
                     'username': username,
                     'email': email,
-                    'password': password,
+                    'password': hashedPassword,
                   });
 
                   // navigate to the next screen
